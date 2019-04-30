1220ee56be3009582d61c374c5349a4f5a91ec1d
ddeana <deanasuncion@gmail.com>
2019-04-30 12:09:22 -0500

    Add files via upload
    Added method to deal 1 and 2 cards.

--- Deck.java
+++ Deck.java
@@ -19,4 +19,42 @@ public class Deck {
             }
         }
     }
+    
+    public void shuffleDeck() {
+    	Collections.shuffle(this.deck);
+    }
+    
+    public ArrayList<Card> dealFirstTwoCards(){
+
+        Card tempCard;
+        ArrayList<Card> tempArray = new ArrayList<Card>(2);
+        ArrayList<Card> deckTemp = this.deck;
+
+
+        for(int i = 0; i < 2; i++){
+            tempCard = deckTemp.get(0);
+            tempArray.add(tempCard);	//Place the topmost card on a new array.
+            deckTemp.remove(0);			//Remove the card that was taken from the top of the deck
+        }
+
+        updateDealerDeck(deckTemp);		//Update the deck after cards are removed 
+        return tempArray;				//Return the 2 cards and deal it to the players
+    }
+    
+    public Card dealOneCard() {
+    	
+    	Card tempCard;
+    	ArrayList<Card> deckTemp = this.deck;
+    	
+    	tempCard = deckTemp.get(0);
+    	deckTemp.remove(0);
+    	
+    	updateDealerDeck(deckTemp);
+    	
+    	return tempCard;
+    }
+    
+    public void updateDealerDeck(ArrayList<Card> c){
+        this.deck = c;
+    }
 }
