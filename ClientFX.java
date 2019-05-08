5bd1387939dd659d0d465b4a5de58477656a87b9
ddeana <deanasuncion@gmail.com>
2019-05-08 06:14:59 -0500

    Handle game flow logic

--- ClientFX.java
+++ ClientFX.java
@@ -1,10 +1,3 @@
-/**
-  * Piotr Ramza
- * pramza2
- * 663328597
- * 
- * CS 342 Project 5
- */
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
@@ -39,7 +32,7 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ClientFX extends Application{
-	
+
 	Label welcome, money, sign, pot;
 	Button setIPandPort, quit, hit, pass, bet, enterName, play, fold, draw;
 	TextField betAmount, potAmount, moneyAmount;
@@ -49,57 +42,58 @@ public class ClientFX extends Application{
 	private TextArea playMessages = new TextArea();
 	boolean stop, first1, first2, first3;
 	String playerName = "";
+	boolean hasFold = false;
 	private final ObservableList<String> opponentList = FXCollections.observableArrayList();
 	ListView<String> opponentListView = new ListView<String>();
 	private Integer score = 0;
 	Label scoreCounter = new Label(score.toString());
-	
+
 	Card op1Card, op2Card, op3Card;
-	
+
 	ArrayList<Card> playerCards = new ArrayList<Card>();
 	ArrayList<String> opponents = new ArrayList<String>();
-	
+
 	HBox cardImages = new HBox();
 	HBox opponentCards1;
 	HBox opponentCards2;
 	HBox opponentCards3;
-	
+
 	private Client conn;
-	
+
 	public String getName() {
 		return playerName;
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void stop() throws Exception{
 		conn.send(playerName + " Quit");
 		stop = true;
 	}
-	
+
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		score = 0;
 		stop = false;
-		
+
 		primaryStage.setTitle("BlackJack!");
-		
+
 		first1 = true;
 		first2 = true;
 		first3 = true;
-		
+
 		cardImages.setAlignment(Pos.CENTER);
-		
+
 		hit = new Button("Hit");
 		hit.setPrefSize(50, 30);
 		pass = new Button("Pass");
 		pass.setPrefSize(50, 30);
 		draw = new Button("Draw");
 		draw.setPrefSize(50, 30);
-		
+
 		fold = new Button("Fold");
 		fold.setPrefSize(50, 30);
 		bet = new Button("Bet");
@@ -108,38 +102,38 @@ public class ClientFX extends Application{
 		sign.setFont(Font.font(20));
 		betAmount = new TextField("10");
 		betAmount.setPrefWidth(50);
-		
+
 		money = new Label("You have:");
 		moneyAmount = new TextField("1000");
 		moneyAmount.setPrefWidth(60);
 		moneyAmount.setEditable(false);
-		
+
 		pot = new Label("Money in the pot:");
 		potAmount = new TextField("0");
 		potAmount.setMaxWidth(60);
 		potAmount.setEditable(false);
-		
+
 		playMessages.setPrefWidth(50);
 		playMessages.setMaxWidth(50);
 		playMessages.setMaxSize(300, 200);
 		playMessages.setEditable(false);
-		
+
 		quit = new Button("Quit");
 		setIPandPort = new Button("Set IP and Port");
-		
+
 		myStage = primaryStage;
-		
-		BorderPane playPane = playScreen();
+
+		BorderPane playPane = playScreen(primaryStage);
 		BorderPane namePane = nameScreen(primaryStage);
-		
+
 		nameScene = new Scene(namePane, 600, 400);
 		playScene = new Scene(playPane, 1000, 1000);
-		
+
 		primaryStage.setScene(nameScene);
-		primaryStage.show();	
-		
+		primaryStage.show();
+
 	}
-	
+
 	//Create the Screen where the player chooses a name
 	private BorderPane nameScreen(Stage pStage) {
 		BorderPane tempPane = new BorderPane();
@@ -149,14 +143,14 @@ public class ClientFX extends Application{
 		Label askName = new Label("Please enter the name you want displayed.");
 		enterName = new Button("Set Name");
 		play = new Button("Ready to Play!");
-		//First prompt Player to connect to a server		
+		//First prompt Player to connect to a server
 		Label ipPortPrompt = new Label("Please input your IP and Port:");
 		TextField clientIPIn = new TextField("127.0.0.1");
 		clientIPIn.setPrefWidth(70);
 		TextField clientPortIn = new TextField("5555");
 		clientPortIn.setPrefWidth(50);
 		Button setIPandPort = new Button("Connect");
-		
+
 		//What happens when the player clicks the Connect button to connect to the server
 		EventHandler<ActionEvent> clickSetIPPort = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
@@ -182,8 +176,8 @@ public class ClientFX extends Application{
 		setIPandPort.setOnAction(clickSetIPPort);
 		HBox ipAndPort = new HBox(10, clientIPIn, clientPortIn, setIPandPort);
 		VBox ipPortAll = new VBox(10, ipPortPrompt, ipAndPort);
-		
-		
+
+
 		//What happens when you enter a name
 		EventHandler<ActionEvent> clickEnterName = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
@@ -194,7 +188,7 @@ public class ClientFX extends Application{
 				}
 				else {
 					try {
-						//sets player name and sends it to the server to add to player list 
+						//sets player name and sends it to the server to add to player list
 						//and check if the name is already taken
 						playerName = name.getText();
 						conn.send("New: " + playerName);
@@ -205,7 +199,7 @@ public class ClientFX extends Application{
 				}
 			}
 		};
-		
+
 		//What happens when you enter a name
 		EventHandler<ActionEvent> clickPlay = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
@@ -213,7 +207,7 @@ public class ClientFX extends Application{
 					playMessages.appendText("You Are: " + playerName + "!!!\n");
 					pStage.setScene(playScene);
 					conn.send("ReadyToPlay: " + playerName);
-				
+
 				} catch (Exception e) {
 					nameMessages.appendText("Could not send name\n");
 				}
@@ -222,55 +216,55 @@ public class ClientFX extends Application{
 		enterName.setOnAction(clickEnterName);
 		play.setOnAction(clickPlay);
 		play.setDisable(true);
-		
+
 		HBox nameLine = new HBox(10, name, enterName);
 		VBox nameAll = new VBox(10 , askName, nameLine);
 		Label opLabel = new Label("Connected Players:");
 		VBox opponentBox = new VBox(10, opLabel, opponentListView);
-		
+
 		//set the middle to prompts and buttons
 		VBox all = new VBox(20, ipPortAll, nameAll, play, nameMessages);
 		tempPane.setCenter(all);
-		
+
 		//set right to Player list
 		tempPane.setRight(opponentBox);
 		opponentListView.setMaxWidth(200);
-		
+
 
 		enterName.setDisable(true);
-		
+
 		return tempPane;
 	}
-	
-	private BorderPane playScreen(){
-		
+
+	private BorderPane playScreen(Stage pStage){
+
 		//Initialize the Border pane that will be returned
 		BorderPane pane1 = new BorderPane();
 		pane1.setPadding(new Insets(5, 5, 5, 5));
-		
-		
+
+
 		//set a background color of green with a black background
 		Rectangle blackBG = new Rectangle(1000, 1000, Color.BLACK);
 		Rectangle greenBG = new Rectangle(980, 980, Color.GREEN);
 		greenBG.setArcHeight(50);
 		greenBG.setArcWidth(50);
-		
+
 		StackPane allBG = new StackPane(blackBG, greenBG);
-		
+
 		VBox background = new VBox(allBG);
 		pane1.getChildren().add(background);
 		//Finish setting up the background
-		
+
 		//Items for the center of the pane
 		Label messageLabel = new Label("Messages");
 		VBox potBox = new VBox(10, pot, potAmount, messageLabel, playMessages);
 		potBox.setAlignment(Pos.CENTER);
 		pane1.setCenter(potBox);
-		
+
 		//Finish Items for the center of the pane
-		
+
 		//Set player UI
-		
+
 		HBox moneyBox = new HBox(8, money, moneyAmount);
 		money.setFont(Font.font(30));
 		moneyBox.setAlignment(Pos.CENTER);
@@ -285,31 +279,31 @@ public class ClientFX extends Application{
 		VBox playerUI = new VBox(10, you, cardImages, moneyBox, inputs, walkAway);
 		playerUI.setAlignment(Pos.CENTER);
 		//Finish player UI
-		
+
 		//Set opponent UI
-		
+
 		opponentCards1 = new HBox(2);
 		opponentCards1.setAlignment(Pos.CENTER);
-		
+
 		pane1.setRight(opponentCards1);
-		
+
 		opponentCards2 = new HBox(2);
 		opponentCards2.setAlignment(Pos.CENTER);
-		
+
 		pane1.setLeft(opponentCards2);
-		
+
 		opponentCards3 = new HBox(2);
 		opponentCards3.setAlignment(Pos.CENTER);
-		
+
 		pane1.setTop(opponentCards3);
-		
+
 		EventHandler<ActionEvent> clickHit = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
 				playMessages.appendText("You chose to hit!\n");
 				try {
-					
+
 					conn.send(playerName + " chose to hit\n");
-					
+
 				} catch (Exception e) {
 					playMessages.appendText("Could not hit\n");
 					System.out.println(e.getMessage());
@@ -318,10 +312,10 @@ public class ClientFX extends Application{
 		};
 		EventHandler<ActionEvent> clickPass = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
-				
+
 				playMessages.appendText("You Passed!\n");
-				
-				try {	
+
+				try {
 					conn.send(playerName + " chose to pass\n");
 					disableAll();
 				} catch (Exception e) {
@@ -332,16 +326,20 @@ public class ClientFX extends Application{
 		};
 		EventHandler<ActionEvent> clickDraw = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
-				
+
 				playMessages.appendText("You drew your first two cards!\n");
-				
-				try {	
+
+				try {
 					conn.send(playerName + " chose to hit\n");
 					conn.send(playerName + " chose to hit\n");
-					
+
 					draw.setDisable(true);
+
+					//enableAll();
 					
-					enableAll();
+					bet.setDisable(false);
+					fold.setDisable(false);
+					
 				} catch (Exception e) {
 					playMessages.appendText("Could not draw your first two cards\n");
 					System.out.println(e.getMessage());
@@ -350,11 +348,13 @@ public class ClientFX extends Application{
 		};
 		EventHandler<ActionEvent> clickFold = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
-				
+
 				playMessages.appendText("You chose to fold!\n");
-				
+
 				try {
 					conn.send(playerName + " chose to fold\n");
+					hasFold = true;
+					disableAll();
 				} catch (Exception e) {
 					playMessages.appendText("Could not fold\n");
 					System.out.println(e.getMessage());
@@ -378,31 +378,46 @@ public class ClientFX extends Application{
 					playMessages.appendText("You bet " + inBet + "\n");
 					Integer moneyLeft = currentMoney - inBet;
 					moneyAmount.setText(moneyLeft.toString());
+					
+					try {
+						conn.send(playerName + " has placed a bet!\n");
+						bet.setDisable(true);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						playMessages.appendText("Could not bet\n");
+						System.out.println(e.getMessage());
+					}
 				}
-				
-				
 			}
 		};
 		EventHandler<ActionEvent> clickQuit = new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
-				
+				playMessages.appendText(playerName + " Quit");
+				pStage.setScene(nameScene);
+
+				try {
+					conn.send( playerName + " Quit");
+				} catch (Exception e){
+					playMessages.appendText("Could not quit");
+					System.out.println(e.getMessage());
+				}
 			}
 		};
-		
+
 		hit.setOnAction(clickHit);
 		pass.setOnAction(clickPass);
 		fold.setOnAction(clickFold);
 		bet.setOnAction(clickBet);
 		quit.setOnAction(clickQuit);
 		draw.setOnAction(clickDraw);
-		
+
 		disableAll();
-		
+
 		pane1.setBottom(playerUI);
-		
+
 		return pane1;
 	}
-	
+
 	//disables all the buttons On the UI
 	private void disableAll() {
 		hit.setDisable(true);
@@ -418,26 +433,26 @@ public class ClientFX extends Application{
 		fold.setDisable(false);
 		bet.setDisable(false);
 	}
-	
-	
+
+
 	private Client createClient(String tempIP, Integer tempPort) {
-		//the client is sending the data and this decides what will be done with the data 
+		//the client is sending the data and this decides what will be done with the data
 		return new Client(tempIP, tempPort, data -> {
 			Platform.runLater(()->{
 				playMessages.appendText(data.toString() + "\n");
 			});
 		});
 	}
-	
+
 	/* #########################################################################################
 	 * Embedded Client class that actually sets up the connection
 	 * #########################################################################################
 	 */
-	
+
 	public class Client {
 
 		private String ip;
-		private int port; 
+		private int port;
 		Consumer<Serializable> callback;
 		private ConnThread connthread = new ConnThread();
 		public Client(String ip, int port, Consumer<Serializable> callback) {
@@ -450,11 +465,11 @@ public class ClientFX extends Application{
 		public void startConn() throws Exception{
 			connthread.start();
 		}
-		
+
 		public void send(Serializable data) throws Exception{
 			connthread.out.writeObject(data);
 		}
-		
+
 		public void closeConn() throws Exception{
 			connthread.socket.close();
 		}
@@ -469,11 +484,11 @@ public class ClientFX extends Application{
 		protected int getPort() {
 			return this.port;
 		}
-		
+
 		class ConnThread extends Thread{
 			private Socket socket;
 			private ObjectOutputStream out;
-			
+
 			public void run() {
 				try{
 					Socket socket = new Socket(getIP(), getPort());
@@ -482,7 +497,7 @@ public class ClientFX extends Application{
 					this.socket = socket;
 					this.out = out;
 					socket.setTcpNoDelay(true);
-						
+
 					while(!stop) {
 						Serializable data = (Serializable) in.readObject();
 						if(data.toString().equals("Quit")) {
@@ -490,7 +505,7 @@ public class ClientFX extends Application{
 							this.socket.close();
 							break;
 						}
-						
+
 						//When the server gives you the player list
 						// "Players: name1 name2 name3 name4"
 						else if(data.toString().contains("Players: ")) {
@@ -498,23 +513,23 @@ public class ClientFX extends Application{
 							int secondSpace = data.toString().indexOf(" ", 2);
 							int thirdSpace = data.toString().indexOf(" ", 3);
 							int fourthSpace = data.toString().indexOf(" ", 4);
-							
+
 							String name1 = data.toString().substring(firstSpace, secondSpace);
 							String name2 = data.toString().substring(secondSpace, thirdSpace);
 							String name3 = data.toString().substring(thirdSpace, fourthSpace);
 							String name4 = data.toString().substring(fourthSpace);
-							
+
 							opponents.add(name1);
 							opponents.add(name2);
 							opponents.add(name3);
 							opponents.add(name4);
 							opponents.remove(playerName);
-							
+
 							//enableAll();
 							draw.setDisable(false);
 							callback.accept(data);
 						}
-						
+
 						//when the server send "NAME has joined the server!"
 						else if(data.toString().contains("joined the server!")) {
 							int firstSpace = data.toString().indexOf(" ", 1);
@@ -532,22 +547,22 @@ public class ClientFX extends Application{
 							opponentListView.refresh();
 							callback.accept(data);
 						}
-						
+
 						////////////////////////////////////////////////////////
 						else if(data.toString().contains(" has drawn")) {
 							int substringOne = data.toString().indexOf(" ", 1);
-					    	int substringTwo = data.toString().indexOf("drawn");
-					    	int substringThree = data.toString().indexOf("of");
-					    	
-					    	String name = data.toString().substring(0, substringOne);
-					    	String suit = data.toString().substring(substringTwo + 6, substringThree - 1);
-					    	String numString = data.toString().substring(substringThree + 3);
-					    	int number = Integer.parseInt(numString);
-					    	
+							int substringTwo = data.toString().indexOf("drawn");
+							int substringThree = data.toString().indexOf("of");
+
+							String name = data.toString().substring(0, substringOne);
+							String suit = data.toString().substring(substringTwo + 6, substringThree - 1);
+							String numString = data.toString().substring(substringThree + 3);
+							int number = Integer.parseInt(numString);
+
 							Card c = new Card(number, suit);
 							//if its an opponent that drew a card, get the index from the opponent list
 							int opIndex = opponents.indexOf(name);
-							
+
 							if(name.equals(playerName)) {
 								playerCards.add(c);
 								score += c.getScore();
@@ -558,12 +573,12 @@ public class ClientFX extends Application{
 									fold.setDisable(true);
 									pass.setDisable(true);
 								}
-								
+
 								Platform.runLater(()-> {
 									cardImages.getChildren().add(c.getPic());
-								});								
+								});
 							}
-							
+
 							else if(opIndex == 0) {
 								if(first1) {
 									first1 = false;
@@ -623,17 +638,35 @@ public class ClientFX extends Application{
 							}
 						}
 						
+						else if(data.toString().contains("Starting")) {
+							if(hasFold == true) {
+								disableAll();
+							}
+							else {
+								bet.setDisable(true);
+								fold.setDisable(true);
+								hit.setDisable(false);
+								pass.setDisable(false);
+							}
+							/*
+							bet.setDisable(true);
+							hit.setDisable(false);
+							pass.setDisable(false);
+							*/
+							callback.accept(data);
+						}
+
 						///////////////////////////////////////////////////////
-						
-                           //###################################################
-                          //ALWAYS GOES TO CATCH, NEEDS MODIFICATION
+
+						//###################################################
+						//ALWAYS GOES TO CATCH, NEEDS MODIFICATION
 						else if(data.toString().contains(" has accepted")) {
 							int firstSpace = data.toString().indexOf("from");
 							String n = data.toString().substring(firstSpace + 5);
 							if (n.equals(playerName)) {
 								int thirdSpace = data.toString().indexOf(" ", 1);
 								String k = data.toString().substring(0, thirdSpace);
-									send(playerName + " ready for " + k);
+								send(playerName + " ready for " + k);
 							}
 							callback.accept(data);
 						}
@@ -643,9 +676,9 @@ public class ClientFX extends Application{
 							scoreCounter.textProperty().set(score.toString());
 							callback.accept(data);
 						}
-						
+
 					}
-					
+
 				}
 				catch(Exception e) {
 					nameMessages.appendText("Could not connect to server.\nPlease close and try again.\n");
